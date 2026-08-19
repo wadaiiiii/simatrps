1052c23c947746184fcaa32477e81c41404170a6
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-08-19 22:33:42 +0800

    Cast user active status to boolean

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'is_active' => 'boolean',
             'password' => 'hashed',
         ];
     }
