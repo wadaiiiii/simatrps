9955fda1e6d85771ff0dd49700540f43f4f45cc4
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-08-20 08:13:46 +0800

    Load RPS workflow UX refinements

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,6 +8,7 @@ import AppLayout from '@/layouts/app-layout';
 import AuthLayout from '@/layouts/auth-layout';
 import SettingsLayout from '@/layouts/settings/layout';
 import '../css/rps-status-colors.css';
+import '../css/rps-workflow-ux.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
