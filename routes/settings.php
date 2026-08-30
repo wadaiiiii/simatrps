4b059a9ab463dcf62d4dac4c1f44fdab9d3ed76a
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-08-30 17:36:39 +0800

    Register SiMatRPS SSO routes

--- routes/settings.php
+++ routes/settings.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Settings\ProfileController;
 use App\Http\Controllers\Settings\SecurityController;
+use App\Http\Controllers\SsoController;
+use App\Http\Middleware\EnsureUserIsActive;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
@@ -23,3 +25,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::inertia('settings/appearance', 'settings/appearance')->name('appearance.edit');
 });
+
+Route::get('sso/authorize', [SsoController::class, 'authorize'])
+    ->middleware(['auth', 'verified', EnsureUserIsActive::class, 'throttle:60,1'])
+    ->name('sso.authorize');
+
+Route::post('sso/token', [SsoController::class, 'token'])
+    ->middleware('throttle:30,1')
+    ->name('sso.token');
